4dae26c5a667ec1865d743f15aec4f612073c882
zigfrid <zigfrid@zigfrid-530U3C-530U4C>
2019-06-05 23:14:01 +0300

    Multithreading sort parts.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <thread>
+#include <mutex>
 
 // Файл для функций и классов работы со временем.
 #include <chrono>
@@ -30,6 +32,90 @@ void SaveSorted(vector<uint32> &arrayForSort, int fileCounter,
   ostream.write(reinterpret_cast<char*>(arrayForSort.data()), numElements*sizeof(uint32));
 }
 
+mutex fileCounterMutex;
+
+void SortPartFile(std::ifstream &input, std::vector<uint32> &buffer, int & fileCounter, int &retflag)
+{
+  //в функции поток читает файл в буфер, увеличивает счётчик (после этого может начать работать другой поток), сортирует файл
+//если конец файла, то сортируем то, что есть
+  fileCounterMutex.lock();
+  input.read(reinterpret_cast<char*>(buffer.data()), buffer.size() * sizeof(uint32));
+  if (!input.eof())
+  {
+    SaveSorted(buffer, ++fileCounter, buffer.size());
+    fileCounterMutex.unlock();
+  }
+  else
+  {
+    size_t numElements = static_cast<size_t>(input.gcount() / sizeof(uint32));
+    SaveSorted(buffer, ++fileCounter, numElements);
+    fileCounterMutex.unlock();
+    { retflag = 2; return; };
+  }
+}
+
+/*
+class SortedFileData
+{
+  ifstream inputFile;
+  int counter = 1;
+  string fileName;
+};*/
+
+bool Method(int& fileCounter)
+{
+  //открыть файл на чтение
+  ifstream input("input", ios::binary);
+  if (!input.is_open())
+  {
+#ifdef WIN32
+    system("PAUSE");
+#endif
+    return false;//FIXME выбросить исключение
+  }
+  //создать буфер
+  vector<uint32> buffer(ARRAY_SIZE);
+  //создать 6 потоков
+
+  //list<SortedFileData> fileDatas(6);//кол-во потоков из зад-я
+  unsigned long const hardwareThreads = thread::hardware_concurrency();
+
+  struct stat fi;
+  stat("input", &fi);
+  unsigned long const maxThreads = static_cast<long>(4 * ARRAY_SIZE + fi.st_size) / (4 * ARRAY_SIZE);
+
+  unsigned long const numThreads = min(hardwareThreads != 0 ? hardwareThreads : 2, maxThreads);
+
+  vector<thread> threads(numThreads);
+  int retFlag = 1;
+  for (unsigned long i = 0; i < numThreads; ++i)
+  {
+    //каждому потоку передать позицию в файле, буфер, счетчик файлов
+    threads[i] = thread{ SortPartFile, ref(input), buffer, ref(fileCounter), ref(retFlag) };
+  }
+
+  //дожидаемся завершения всех потоков
+  for_each(threads.begin(), threads.end(), mem_fn(&thread::join));
+
+  //главный поток должен проверять, если у какого-то потока закончился кусок файла, то дать ещё работу. Тогда заменить numThreads - 1
+  //если пришли в деструктор потока и не конец файла, то дать ещё работу.
+
+ for (;;)
+ {
+   for (unsigned long i = 0; i < numThreads; ++i)
+   {
+     //каждому потоку передать позицию в файле, буфер, счетчик файлов
+     if(!threads[i].joinable() && retFlag != 2)
+       threads[i]{ SortPartFile, ref(input), buffer, ref(fileCounter), ref(retFlag) };
+//     if (retFlag == 2)
+//       break;
+   }
+//   if (retFlag == 2)
+//    break;
+ }
+
+  return true;
+}
 
 bool SortParts(int& fileCounter)//сортировка частей файла
 {
@@ -44,17 +130,12 @@ bool SortParts(int& fileCounter)//сортировка частей файла
 
   vector<uint32> buffer(ARRAY_SIZE);
   fileCounter = 0;
-  for(;;)
+
+  for (;;)
   {
-    input.read(reinterpret_cast<char*>(buffer.data()), buffer.size()*sizeof(uint32));
-    if(!input.eof())
-      SaveSorted(buffer, ++fileCounter, buffer.size());
-    else
-    {
-      size_t numElements = input.gcount() / sizeof(uint32);
-      SaveSorted(buffer, ++fileCounter, numElements);
-      break;
-    }
+    int retflag;
+    SortPartFile(input, buffer, fileCounter, retflag);
+    if (retflag == 2) break;
   }
   return true;
 }
@@ -77,12 +158,12 @@ void MergeParts(int fileCounter)
 
   list<MergedFileData> fileDatas(fileCounter);
   int fileNumber = 1;
-  for(auto& fileData : fileDatas)
+  for (auto& fileData : fileDatas)
   {
     fileData.fileName = "output" + to_string(fileNumber);
     ++fileNumber;
     fileData.inputFile.open(fileData.fileName, ios::binary);
-    if(!fileData.inputFile.is_open())
+    if (!fileData.inputFile.is_open())
       return;//FIXME переделать на исключение
 
     fileData.inputFile.read(reinterpret_cast<char*>(&fileData.number), sizeof(uint32));
@@ -98,17 +179,17 @@ void MergeParts(int fileCounter)
 
     if (outPos >= ARRAY_SIZE)
     {
-       output.write(reinterpret_cast<char*>(outData.data()), outData.size()*sizeof(uint32));
-       outPos = 0;
+      output.write(reinterpret_cast<char*>(outData.data()), outData.size()*sizeof(uint32));
+      outPos = 0;
     }
 
     outData[outPos] = itMin->number;
     ++outPos;
 
     itMin->inputFile.read(reinterpret_cast<char*>(&itMin->number), sizeof(uint32));
-    if(itMin->inputFile.eof())
+    if (itMin->inputFile.eof())
     {
-      const string fileName (itMin->fileName);
+      const string fileName(itMin->fileName);
       fileDatas.erase(itMin);
       remove(fileName.c_str());
     }
@@ -120,7 +201,7 @@ void MergeParts(int fileCounter)
 
   output << lastFile.inputFile.rdbuf();
 
-  const string fileName (lastFile.fileName);
+  const string fileName(lastFile.fileName);
   fileDatas.clear();
   remove(fileName.c_str());
 
@@ -140,9 +221,13 @@ int main()
   // Получаем текущее время, используя высокоточный таймер.
   auto t1 = chrono::high_resolution_clock::now();
 
-  int fileCounter = 1;
-  if (!SortParts(fileCounter))
+  int fileCounter = 0;
+
+  if (!Method(fileCounter))
     return 1;
+
+//  if (!SortParts(fileCounter))
+//    return 1;
 
   // Снова получаем текущее время после выполнения операции.
   auto t2 = chrono::high_resolution_clock::now();
@@ -178,15 +263,10 @@ int main()
   // Отображаем результаты.
   cout << "Sort temp files: " << elapsed1.count() << endl;
   cout << "Merge temp files: " << elapsed2.count() << endl;
-  cout << "Total time: "      << elapsed3.count() << endl;
+  cout << "Total time: " << elapsed3.count() << endl;
 
   //FIXME сделать, чтобы по завершении работы программы сгенерированные файлы удалялись (оставался только output)
 #ifdef WIN32
   system("PAUSE");
 #endif
 }
-
-
-
-
-
